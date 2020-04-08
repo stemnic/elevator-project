cb0244574f56881ecfe1df88882f916018b9709e
Ole Sivert Aarhaug <ole.sivert@aarhaug.com>
2020-04-08 17:55:19 +0200

    Removed commented code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,34 +7,6 @@ mod tasks;
 mod elev_controller;
 
 fn main() {
-    /*
-    println!("Starting");
-    let mut controller = elev_controller::ElevController::new().unwrap();
-    println!("Done init!");
-    controller.add_order(elev_controller::Order{floor: 3, order_type: elev_controller::ElevatorActions::Cabcall});
-    controller.add_order(elev_controller::Order{floor: 0, order_type: elev_controller::ElevatorActions::Cabcall});
-    controller.add_order(elev_controller::Order{floor: 2, order_type: elev_controller::ElevatorActions::Cabcall});
-    let (sender, reciver) = channel::<elev_controller::ElevatorButtonEvent>();
-    thread::spawn(move || {
-        let socket = network_rust::bcast::BcastReceiver::new(elev_controller::BCAST_PORT).unwrap();
-        
-        thread::spawn(move || {
-            socket.run(sender);
-        });
-    });
-    loop {
-        controller.handle_order();
-        controller.check_buttons();
-        match reciver.try_recv() {
-            Ok(value) => {
-                controller.set_button_light_for_order(value.action, elev_driver::Floor::At(value.floor));
-                let new_order = elev_controller::Order{floor: value.floor, order_type: elev_controller::ElevatorActions::Cabcall};
-                controller.add_order(new_order);
-            }
-            Err(_) => {}
-        }
-    }
-    */
     let (network_sender, network_reciver) = channel::<elev_controller::ElevatorButtonEvent>();
     thread::spawn(move || {
         let socket = network_rust::bcast::BcastReceiver::new(elev_controller::BCAST_PORT).unwrap();
