9aea3866d104e080ffb9a62a0190aa93d4c0669c
bjornrho <bjornrho@ntnu.no>
2020-04-17 14:47:10 +0200

    Shady cab call solution

--- src/elev_controller.rs
+++ src/elev_controller.rs
@@ -104,9 +104,11 @@ impl ElevController {
                     self.driver.set_floor_light(Floor::At(c_floor)).unwrap();
                     let mut clear_orders_at_floor: std::vec::Vec<Order> = vec![]; //used to clear all orders at the floor the elevator arrives at
                     let queue_clone=self.queue.clone();
-                    match self.queue.front() {
+                    let queue_clone1=self.queue.clone();
+                    let mut shady_shit = true;
+                    match self.queue.front(){
                         Some(order) => {
-                            //println!("[elev_controller] C: {:?} O: {:?}", c_floor, order.floor);   
+                            //println!("[elev_controller] C: {:?} O: {:?}", c_floor, order.floor);
                             if c_floor > order.floor{
                                 self.driver.set_motor_dir(MotorDir::Down).expect("Set MotorDir failed");
                             }
@@ -116,12 +118,13 @@ impl ElevController {
                             if c_floor == order.floor{
                                 self.driver.set_motor_dir(MotorDir::Stop).expect("Set MotorDir failed");
                                 for task in queue_clone{
-                                    if task.floor ==c_floor{
+                                    if task.floor == c_floor{
                                     clear_orders_at_floor.push(task.clone());
                                     }
                                 }
                                 self.complete_order_signal(order);
                                 self.open_door();
+                                shady_shit = false;
                             }
                             match self.last_floor {
                                 Floor::At(p_floor) => {
@@ -137,6 +140,23 @@ impl ElevController {
                         }
                         None => {
                             self.driver.set_motor_dir(MotorDir::Stop).unwrap();
+                        }
+                    }
+                    if shady_shit{
+                        for task in queue_clone1{
+                            match task.order_type{
+                                ElevatorActions::Cabcall => {
+                                    if task.floor == c_floor{
+                                        self.driver.set_motor_dir(MotorDir::Stop).expect("Set MotorDir failed");
+                                        let index = self.queue.iter().position(|x| *x == task).unwrap();
+                                        self.queue.remove(index);
+                                        self.complete_order_signal(&task);
+                                        self.open_door();
+                                        println!("[Elev_controller]: Stopped here!")
+                                    }
+                                }
+                                _ => println!("")
+                            }
                         }
                     }
                     for task in clear_orders_at_floor {
